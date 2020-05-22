ecf8680f000e9949d18632bcef3f9c753a36e476
Aristide BESNIER <aristidebesnier@Aristides-MacBook-Pro.local>
2020-05-22 12:44:44 +0200

    last leak verification

--- ft_assign.c
+++ ft_assign.c
@@ -12,44 +12,36 @@
 
 #include "libft_printf.h"
 
-static void		ft_size_2(char *s1, char *s2, char *s3, t_list *elem)
+static void		ft_size_2(char *s1, t_list *elem)
 {
-	if (elem->conv == 'x')
-	{
-		s3 = ft_uitoa_base(elem->u_integer, s1);
-		elem->size = ft_strlen(s3);
-		free(s3);
-	}
-	else if (elem->conv == 'X')
-	{
-		s3 = ft_uitoa_base(elem->u_integer, s2);
-		elem->size = ft_strlen(s3);
-		free(s3);
-	}
-	else if (elem->conv == 'u')
-	{
-		s3 = ft_itoa(elem->u_integer);
-		elem->size = ft_strlen(s3);
-		free(s3);
-	}
-	else if (elem->conv == 'i' || elem->conv == 'd')
-	{
-		s3 = ft_itoa(elem->integer);
-		elem->size = ft_strlen(s3);
-		free(s3);
-	}
+	elem->size = ft_strlen(s1);
+	free(s1);
 }
 
 static void		ft_size(t_list *elem)
 {
 	char		*s1;
-	char		*s2;
-	char		*s3;
 
-	s1 = "0123456789abcdef";
-	s2 = "0123456789ABCDEF";
-	s3 = NULL;
-	ft_size_2(s1, s2, s3, elem);
+	if (elem->conv == 'x')
+	{
+		s1 = ft_uitoa_base(elem->u_integer, "0123456789abcdef");
+		ft_size_2(s1, elem);
+	}
+	else if (elem->conv == 'X')
+	{
+		s1 = ft_uitoa_base(elem->u_integer, "0123456789ABCDEF");
+		ft_size_2(s1, elem);
+	}
+	else if (elem->conv == 'u')
+	{
+		s1 = ft_itoa(elem->u_integer);
+		ft_size_2(s1, elem);
+	}
+	else if (elem->conv == 'i' || elem->conv == 'd')
+	{
+		s1 = ft_itoa(elem->integer);
+		ft_size_2(s1, elem);
+	}
 }
 
 static void		ft_assign_2(t_list *elem)
